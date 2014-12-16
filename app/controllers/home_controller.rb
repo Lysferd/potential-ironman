fb596b890fe28f392a827f69e1dbb640ed2327c0
Fabio Almeida <fabioalmeida@ibtecnologia.com.br>
2014-12-16 18:21:31 -0200

    Using HAML, DRYing viewers, mobile/tablet versions.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,9 @@
 class HomeController < ApplicationController
 
-  #before_action :check_user, only: :index
-  #skip_before_action :require_login, only: [ :index, :create ]
-
   def index
+    #render layout: 'mobile' if params[:mobile]
   end
 
   def admin
   end
-  
-=begin
-  def create
-    if user = User::authenticate( params[:username], params[:password] )
-      session[:user_id] = user.id
-      redirect_to admin_url
-    else
-      redirect_to login_url, alert: 'Invalid login.'
-    end
-  end
-  
-  def destroy
-    session[:user_id] = nil
-    redirect_to login_url, notice: 'Logged out.'
-  end
-
-  private
-  def check_user
-    redirect_to admin_url if session[:user_id]
-  end
-=end
-
 end
