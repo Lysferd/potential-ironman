6fdfc2845064708d1af284ad93643d51aca60ef0
Vitor Hugo Cardoso <hcardoso.vitor@gmail.com>
2015-01-21 11:36:00 -0200

    Version and Dependencies added to product.
    * Added Version to products.
    * Added Dependencies to products.
    * Added helper method `full_label` in product model, both Dependencies
    and Compatible Products are using it.
    * Added the translation to `product_dependencies_labels`.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,10 @@ class Product < ActiveRecord::Base
   belongs_to :manufacturer
   #has_one :product_type
 
-  validates :label, presence: true, uniqueness: true
+  validates :label, presence: true
   validates :manufacturer_id, presence: true
+
+  before_validation :remove_empty_elements
 
   def manufacturer
     Manufacturer::find( self.manufacturer_id ).label
@@ -14,7 +16,7 @@ class Product < ActiveRecord::Base
     return I18n::t( :empty_field ) if self.product_whitelist.empty?
     labels = [ ]
     self.product_whitelist.each do |p|
-      labels.push( Product::find( p ).label )
+      labels.push( Product::find( p ).full_label )
     end
     return labels.join( ', ' )
   end
@@ -27,10 +29,29 @@ class Product < ActiveRecord::Base
     end
     return labels.join( ', ' )
   end
+  
+  def product_dependencies_labels
+    return I18n::t( :empty_field ) if self.product_dependencies.empty?
+    labels = [ ]
+    self.product_dependencies.each do |p|
+      labels.push( Product::find( p ).full_label )
+    end
+    return labels.join( ', ' )
+  end
+
+  def full_label
+    return self.label + " #{self.version}"
+  end
 
   def short_description
     return self.description if self.description.size <= 100
     self.description[0...100] + '(...)'
+  end
+
+  private
+  def remove_empty_elements
+    self.platform_whitelist.delete( '0' )
+    self.product_whitelist.delete( '0' )
   end
   
 end
