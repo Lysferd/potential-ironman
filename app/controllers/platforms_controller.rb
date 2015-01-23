a1f7546ad157b2d3c79339948e641a408dface83
Vitor Hugo Cardoso <hcardoso.vitor@gmail.com>
2015-01-23 11:46:47 -0200

    Fixed Product/Platform Version Property
    * Issue Product Version Property done.
    * `version´ added to platform.
    * Solution uses `full_label´ for both Product and Platforms.
    
    * fixes #8

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -71,6 +71,6 @@ class PlatformsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def platform_params
-    params.require(:platform).permit(:label, :version, :description, :manufacturer_id)
+    params.require(:platform).permit(:label, :version,  :description, :manufacturer_id)
   end
 end
